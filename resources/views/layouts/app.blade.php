ee5b2026a6b326bdc9890185e288ead8dd835fa4
sonthth1903012 <sonthth1903012@fpt.edu.vn>
2020-05-02 02:24:22 +0700

    update form login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
                 <div class="col-12 h-100">
                     <div class="header-content h-100 d-flex align-items-center justify-content-between">
                         <div class="academy-logo">
-                            <a href="{{url('/')}}"><img src="img/core-img/logo.png" alt=""></a>
+                            <a href="{{url('/')}}"><img src={{asset("img/core-img/logo.png")}} alt=""></a>
                         </div>
                             <div class="login-content">
                                 <a href="{{url('/login')}}" style="padding-right:10px">Login</a>
