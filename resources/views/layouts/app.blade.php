4f1f63c91b203f0392fa32cf911a8c6bbd85633f
sonthth1903012 <sonthth1903012@fpt.edu.vn>
2020-05-02 21:11:32 +0700

    update layout login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,16 +92,23 @@
 
 
     @includeIf('html.footer')
+
+    <!-- ##### Preloader ##### -->
+    <div id="preloader">
+        <i class="circle-preloader"></i>
+    </div>
+
+
     <!-- ##### All Javascript Script ##### -->
     <!-- jQuery-2.2.4 js -->
-    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <script src={{asset("js/jquery/jquery-2.2.4.min.js")}}></script>
     <!-- Popper js -->
-    <script src="js/bootstrap/popper.min.js"></script>
+    <script src={{asset("js/bootstrap/popper.min.js")}}></script>
     <!-- Bootstrap js -->
-    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src={{asset("js/bootstrap/bootstrap.min.js")}}></script>
     <!-- All Plugins js -->
-    <script src="js/plugins/plugins.js"></script>
+    <script src={{asset("js/plugins/plugins.js")}}></script>
     <!-- Active js -->
-    <script src="js/active.js"></script>
+    <script src={{asset("js/active.js")}}></script>
 </body>
 </html>
