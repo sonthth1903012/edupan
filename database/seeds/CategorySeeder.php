e279d0a251018147b28141b201f0ff467135378f
sonthth1903012 <sonthth1903012@fpt.edu.vn>
2020-05-15 05:07:45 +0700

    pg

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
+class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class,5)->create();
+        factory(\App\Category::class,5)->create();
     }
 }
--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
+class PostSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class,5)->create();
+        factory(\App\Post::class,20)->create();
     }
 }
